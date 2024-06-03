4c2b30c6fa9ec0e70280868fd8aa79f9fb0b3362
Nicholas Manoharan <134759240+nicholasmanoharan@users.noreply.github.com>
2024-06-03 15:56:22 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,46 +1,34 @@
-#include <iostream> 
+#include <iostream>
 #include <unordered_map>
-#include <vector> 
+#include <vector>
 #include <set>
 #include <limits>
-#include <algorithm> 
+#include <algorithm>
 using namespace std;
 
-class Graph { 
-
+class Graph {
     typedef unordered_map<string, unordered_map<string, int> > AdjMap;
-    AdjMap adj; 
+    AdjMap adj;
 
+public:
+    void addVertex(const string &label) {
+        adj[label];
+    }
 
-    public: 
-        void addVertex(const string &label) { 
+    void addEdge(const string &from, const string &to, int weight) {
+        adj[from][to] = weight;
+    }
 
-            adj[label]; 
-
+    void removeVertex(const string &label) {
+        adj.erase(label);
+        for (AdjMap::iterator it = adj.begin(); it != adj.end(); ++it) {
+            it->second.erase(label);
         }
+    }
 
-        void addEdge(const string &from, const string &to, int weight) { 
-
-            adj[from][to] = weight;
-
-        }
-
-        void removeVertex(const string &label) { 
-
-            adj.erase(label); 
-            for (AdjMap::iterator it = adj.begin(); it != adj.end(); ++it) { 
-                
-                it->second.erase(label); 
-
-            }
-
-        }
-
-        void removeEdge(const string &from, const string &from, const string &to) {
-
-            adj[from].erase(to); 
-
-        }
+    void removeEdge(const string &from, const string &to) {
+        adj[from].erase(to);
+    }
 
     void printTable() {
         for (AdjMap::const_iterator it = adj.begin(); it != adj.end(); ++it) {
@@ -52,85 +40,65 @@ class Graph {
         }
     }
 
-    void shortestPath(const string &start, const string &end) { 
-
+    void shortestPath(const string &start, const string &end) {
         typedef unordered_map<string, int> DistMap;
-        typedef unordered_map< string, string> PrevMap; 
-        typedef set<pair<int, string> > PQ; 
+        typedef unordered_map<string, string> PrevMap;
+        typedef set<pair<int, string> > PQ;
 
-        DistMap dist; 
-        DistMap prev; 
-        PQ pq; 
+        DistMap dist;
+        PrevMap prev;
+        PQ pq;
 
-        for (AdjMap::const_iterator it = adj.begin()l it != adk.end(); ++it) {
-
-            dist[it->first] = numeric_limits<int>::max(); 
-
+        for (AdjMap::const_iterator it = adj.begin()l it != adj.end(); ++it) {
+            dist[it->first] = numeric_limits<int>::max();
         }
+        dist[start] = 0;
+        pq.insert(make_pair(0, start));
 
-        dist[start] = 0; 
-        pq.insert(make_pair(0, start)); 
-
-        while (!pq.empty()) { 
+        while (!pq.empty()) {
             string u = pq.begin()->second;
             pq.erase(pq.begin());
 
-            if (u == end) break; 
+            if (u == end) break;
 
-            for (unordered_map<string, int>::const_iterator it = adj[u].begin; it != adj[u].end(); ++it) { 
-                int alt = dist[u] + it->second; 
-                if (alt < dist[it->first]) { 
-                    pq.erase(make_pair(dist[it->first], it ->first));
-                    dist[it->first] = alt; 
-                    prev[it->first] = u; 
-                    pq.insert(make_pair(alt, it->first)); 
-
-                    
+            for (unorred_map<string, int>::const_iterator it = adj[u].begin(); it != adj[u].end(); ++it) {
+                int alt = dist[u] + it->second;
+                if (alt < dist[it->first]) {
+                    pq.erase(make_pair(dist[it->first], it->first));
+                    dist[it->first] = alt;
+                    prev[it->first] = u;
+                    pq.insert(make_pair(alt, it->first));
                 }
             }
-
-            if (dist[end] == numeric_limits<int>::max()) { 
-                cout << "No path was found" << endl; 
-                
-            } else { 
-
-                vector<string> path; 
-                for (string at = end; at != start; at = prev[at])
-                    path.push_back(at);
-                path.push_back(start); 
-                reverse(path.begin(), path.end()); 
-
-
-                cout << "Shortest path is: ";
-                for (vector<string>::iterator it = path.begin(); it != path.end(); ++it) 
-                    cout << *it << " "; 
-
-                cout << "\nWeight: " << dist[end] << endl; 
-            
-
-            }
-            
-            
         }
 
-    }
+        if (dist[end] == numeric_limits<int>::max()) {
+            cout << "No path exists" << endl;
+        } else {
+            vector<string> path;
+            for (string at = end; at != start; at = prev[at])
+                path.push_back(at);
+            path.push_back(start);
+            reverse(path.begin(), path.end());
 
+            cout << "Shortest path: ";
+            for (vector<string>::iterator it = path.begin(); it != path.end(); ++it)
+                cout << *it << " ";
+            cout << "\nTotal weight: " << dist[end] << endl;
+        }
+    }
 };
 
-int main() { 
-
-    Graph g; 
+int main() {
+    Graph g;
     string command, from, to, label;
-    int weight; 
+    int weight;
 
+    while (true) {
+        cout << "Commands: add_vertex, add_edge, remove_vertex, remove_edge, print, shortest_path, quit" << endl;
+        cout << "Enter command: ";
+        cin >> command;
 
-    while (true) { 
-
-        cout << "Commands: add_vertex, add_edge, remove_vertex, remove_edge, print, shortest_path, quit" << endl; 
-        cout << "Enter command: "; 
-        cin >> command; 
-
-        
         if (command == "add_vertex") {
             cout << "Enter vertex label: ";
             cin >> label;
@@ -148,19 +116,17 @@ int main() {
             cin >> from >> to;
             g.removeEdge(from, to);
         } else if (command == "print") {
-            g.printAdjTable();
+            g.printTable();
         } else if (command == "shortest_path") {
             cout << "Enter start vertex and end vertex: ";
             cin >> from >> to;
-            g.findShortestPath(from, to);
+            g.shortestPath(from, to);
         } else if (command == "quit") {
             break;
         } else {
             cout << "Invalid command." << endl;
         }
     }
-    
+
     return 0;
-
-
 }
